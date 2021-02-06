c9d26ab0465a648fecb0ff4dbfaedaf8821f0454
Mai Pham <maitphm@gmail.com>
2021-02-06 13:12:15 -0500

    commit tree 4ce93ad481432d281cb3cfc45c19a6f44e467289

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,11 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
       <button type="submit">Submit</button>
     </form>
   );
