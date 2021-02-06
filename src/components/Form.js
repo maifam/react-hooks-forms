672da39246dfb921c3869f5f95f52906902c13fd
Mai Pham <maitphm@gmail.com>
2021-02-06 13:33:35 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,16 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [newsetter, setNewsetter] = useState(false)
+
+
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+      <label htmlFor="newsletter">Subscribe to Our Newsletter</label>
+      <input type="checkbox" checked={newsetter} onChange={(e) => setNewsetter(e.target.checked)}/>
       <button type="submit">Submit</button>
     </form>
   );
